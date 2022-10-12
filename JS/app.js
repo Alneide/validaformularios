8b5cb3685c0d2d91dfe2b38d5d73a70b29a58624
Alneide <109877622+Alneide@users.noreply.github.com>
2022-10-12 18:58:33 -0300

    importamos a função de valida para o arquivo “app.js",

---
+++ JS/app.js
@@ -0,0 +1,9 @@
+import (valida ) from './validacao.js'
+
+const inputs = document.querySelectorAll('input')
+
+inputs.forEach(input=>{
+	input.addEventListener('valida(input: any'):void
+	   valida(evento.target)
+  })
+
